65038aa600676268e7a704f38029776b78f78bc9
RWei <zhiweiiilu@gmail.com>
2025-04-17 22:18:51 +0800

    加入購物車和qty

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -4,9 +4,9 @@ function AddToCart() {
     return (
         <>
             {/* 電腦螢幕*/}
-            <button className="hidden md:flex btn bg-red-300 px-8 py-5 mx-[3.9rem] text-gray-700">
+            <button className="hidden md:flex btn bg-red-300 px-5 py-5 w-[10rem] mx-[3.6rem] text-gray-700">
                 <ShoppingBasket
-                    strokeWidth={1.5} className="w-5 h-5 md:w-6 md:h-6 text-current group-hover:scale-105 transition-transform"
+                    strokeWidth={1.5} className=" w-5 h-5 md:w-6 md:h-6 text-current group-hover:scale-105 transition-transform"
                 />
                 <span className="font-normal ml-3">加入購物車</span>
 
@@ -14,7 +14,7 @@ function AddToCart() {
 
             {/* 手機螢幕*/}
 
-            <button className=" md:hidden btn bg-red-300 pr-[0.3rem]  py-0.5 h-[1.8rem] mr-[4rem] text-gray-700">
+            <button className=" md:hidden btn bg-red-300 pr-[0.3rem] w-[7.5rem] py-0.5 h-[1.8rem] mx-[0.5rem] text-gray-700">
                 <ShoppingBasket
                     strokeWidth={1.5} className="w-4.2 h-4 md:w-6 md:h-6 text-current group-hover:scale-105 transition-transform"
                 />
--- src/components/FoodDetail.jsx
+++ src/components/FoodDetail.jsx
@@ -1,7 +1,11 @@
 import Heart from './Heart'
 import AddToCart from './AddToCart'
+import { useState } from "react"
+
 
 function FoodDetail({ food }) {
+    const [qty, setQty] = useState(1);
+
     return (
         <>
             {/* 電腦螢幕*/}
@@ -20,9 +24,13 @@ function FoodDetail({ food }) {
                         </div>
                     </div>
 
-
-                    <div className="w-full px-4 flex justify-start -mt-3">
-                        <h3 className="text-[2rem]  mx-[3rem] font-semibold">{food.name}</h3>
+                    <div className="flex w-full ">
+                        <div className="w-full px-4 flex justify-start -mt-3 text-left pl-0 pl-[4rem]">
+                            <h3 className="w-[10rem] text-[2rem] font-semibold">{food.name}</h3>
+                        </div>
+                        <div className="w-full px-4 ">
+                            <p className="pr-[4rem] text-right text-[#631616] font-md text-[1.2rem]"> ${food.price}NTD</p>
+                        </div>
                     </div>
 
 
@@ -41,18 +49,37 @@ function FoodDetail({ food }) {
                         >
                         </textarea>
                     </div>
-                    <div className="flex w-full">
-                        <div className="w-full px-4 mb-[2rem]">
-                            <p className="ml-[3rem] text-left text-[#631616] font-md text-[1.2rem]"> ${food.price}NTD</p>
+
+                    <div className="w-full flex justify-between items-center">
+                        <div className=" w-full text-left pl-[4rem] -mt-[2rem]">
+                            <span className="font-bold"> 數量: </span>
+                            <select
+                                className="select select-bordered w-20 bg-gray-300"
+                                value={qty}
+                                onChange={event => setQty(Number(event.target.value))}
+                            >
+                                {[...Array(10).keys()].map((x) => (
+                                    <option key={x + 1} value={x + 1}>
+                                        {x + 1}
+                                    </option>
+                                ))}
+
+                            </select>
                         </div>
-                        <AddToCart />
+                        <div className="mb-[3rem] flex flex-col items-center justify-between pr-8 gap-2 pr-[0.5rem] text-[#631616]">
+                            <p className="w-full text-right px-[3.8rem] text-[1.2rem]">
+                                <span className="font-bold">總價</span>:  {food.price * qty}
+                            </p>
+                            <AddToCart />
+                        </div>
+
                     </div>
                 </div>
             </div>
 
             {/* 手機螢幕*/}
             <div className="md:hidden">
-                <div className="w-3/5 mx-auto flex flex-col justify-center items-center gap-10 bg-[#F9E1E1] text-black  mt-10 mb-[10rem] py-5 rounded-3xl">
+                <div className="w-3/5 mx-auto pr-[1rem] flex flex-col justify-center items-center gap-10 bg-[#F9E1E1] text-black  mt-10 mb-[10rem] py-5 rounded-3xl">
                     <img
                         src={food.pic}
                         alt=""
@@ -60,20 +87,26 @@ function FoodDetail({ food }) {
 
 
                     <div className="w-full px-4 mb-0">
-                        <div className="flex justify-start items-center gap-[1.5rem] mx-[3rem]">
+                        <div className="flex justify-start items-center gap-[1.5rem] mx-[2.5rem]">
                             <Heart />
                             <button className="border border-black border-[2px] rounded-2xl px-[1rem] py-[0.1rem] text-[0.9rem] text-black transition-all duration-400 hover:bg-red-300 hover:border-none hover:text-white cursor-pointer group-hover:scale-105"> Comments </button>
                         </div>
                     </div>
 
 
-                    <div className="w-full px-4 flex justify-start -mt-3">
-                        <h3 className="text-[1.5rem]  mx-[3rem] font-semibold">{food.name}</h3>
+                    <div className="flex w-full">
+                        <div className=" w-[12rem] px-2 flex justify-start -mt-3 text-left pl-0 pl-[4rem] mr-0 -ml-[0.5rem]">
+                            <h3 className="w-[10rem] text-[1.4rem] font-semibold ">{food.name}</h3>
+                        </div>
+                        <div className="w-[5.5rem] px-3 -ml-[1rem]">
+                            <p className="text-left text-[#631616] font-md text-[1rem]"> ${food.price}NTD</p>
+                        </div>
                     </div>
 
 
+
                     <div className="w-full px-4 flex justify-start items-center -mt-3">
-                        <p className="mx-[3rem] text-[1.1rem] w-[8rem]">{food.information}</p>
+                        <p className="mx-[2rem] text-[1.1rem] w-[8rem]">{food.information}</p>
                         <button className="h-[3rem] border w-[4rem] rounded-sm px-[0.7rem] text-[0.8rem] mr-[0.9rem] bg-[#D9D9D9] text-[#424242] border-[#424242] transition-all duration-400 hover:bg-gray-300 hover:border-none hover:text-white cursor-pointer">詳細資訊</button>
                     </div>
 
@@ -87,11 +120,29 @@ function FoodDetail({ food }) {
                         >
                         </textarea>
                     </div>
-                    <div className="flex w-full">
-                        <div className="w-full px-4 mb-[2rem]">
-                            <p className="ml-[3rem] text-left text-[#631616] font-md text-[1rem]"> ${food.price}NTD</p>
+                    <div className="w-full flex justify-between items-center">
+                        <div className=" w-full text-left pl-[4rem] -mt-[2rem]">
+                            <span className="font-bold "> 數量: </span>
+                            <select
+                                className="select select-bordered w-15 bg-gray-300"
+                                value={qty}
+                                onChange={event => setQty(Number(event.target.value))}
+                            >
+                                {[...Array(10).keys()].map((x) => (
+                                    <option key={x + 1} value={x + 1}>
+                                        {x + 1}
+                                    </option>
+                                ))}
+
+                            </select>
                         </div>
-                        <AddToCart />
+                        <div className="mb-[3rem] flex flex-col pr-8 gap-2 pr-[0.5rem] text-[#631616] mt-[0.7rem]">
+                            <p className="w-[8rem] text-right pr-0 text-[1rem]">
+                                <span className="font-bold">總價</span>:  {food.price * qty}
+                            </p>
+                            <AddToCart />
+                        </div>
+
                     </div>
 
                 </div>
