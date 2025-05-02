41f47eb48c85e75a172c2d7406b6661615f0a2c5
RWei <zhiweiiilu@gmail.com>
2025-05-02 15:10:40 +0800

    增加motion

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -1,18 +1,7 @@
-import { useEffect } from "react";
-import gsap from "gsap";
+import { motion } from "framer-motion";
 
 
 function Concept() {
-
-    useEffect(() => {
-        gsap.to(".concept", {
-            x: 0,              // 回到原位
-            opacity: 1,        // 顯示出來
-            duration: 1.8,
-            ease: "power2.out",
-        });
-    }, []);
-
     return (
         <>
             <div className="relative pt-[10rem] h-[40rem] font-bold mb-[15rem]">
@@ -24,20 +13,29 @@ function Concept() {
                 <div className="absolute inset-0 bg-black opacity-35 z-10"></div>
 
                 {/* 內容區塊 */}
-                <div className="relative z-20">
+
+                <motion.div  
+                    initial={{x : "-40%"}}
+                    whileInView={{x:0}}
+                    transition={{
+                        type: "tween",
+                        duration: 2,
+                        ease: "easeInOut",
+                    }}
+                    className="relative z-20"
+                >
 
                     <div className=" text-[2rem] mb-[2rem] text-left pl-[6rem]">
-                        <p className="concept -translate-x-[800px] opacity-0">一份早餐 一段記憶</p>
+                        <p>一份早餐 一段記憶</p>
                     </div>
                     <div className="text-[2rem]  mb-[2rem] text-left pl-[8rem]">
-                        <p className="concept -translate-x-[800px] opacity-0">放慢腳步 享受時光</p>
+                        <p>放慢腳步 享受時光</p>
                     </div>
                     <div className=" text-[2rem] text-left pl-[10rem]">
-                        <p className="concept -translate-x-[800px] opacity-0">好食相伴 祝你好食</p>
+                        <p>好食相伴 祝你好食</p>
                     </div>
-                </div >
+                </motion.div  >
             </div>
-
 
         </>
     )
