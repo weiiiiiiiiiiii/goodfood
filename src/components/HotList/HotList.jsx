c4ea76e817002122cc5629abd1540f2cadb789bf
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2025-05-23 17:29:29 +0800

    修改detail，增加json檔的detail

--- src/components/HotList/HotList.jsx
+++ src/components/HotList/HotList.jsx
@@ -3,20 +3,25 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Scrollbar } from 'swiper/modules';
 import './HotList.css'
 
+
 import 'swiper/css';
 import 'swiper/css/scrollbar';
+
+
 
 
 function HotList({ breakfast }) {
     return (
         <div className="bg-[#F3EBE6] mb-[8rem] pb-20 ">
 
+
             {/* 標題 */}
             <div className="flex pt-15 pb-20 items-center justify-center gap-5">
-                <hr className="text-[#E5C049] w-50 md:w-80 lg:w-150 border-1" />
+                <hr className="text-[#E5C049] w-45 md:w-80 lg:w-150 border-1" />
                 <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-[#E5C049]">人氣美食</h1>
-                <hr className="text-[#E5C049] w-50 md:w-80 lg:w-150 border-1" />
+                <hr className="text-[#E5C049] w-45 md:w-80 lg:w-150 border-1" />
             </div>
+
 
             {/* 滾動顯示人氣商品，用json中的id2項目 */}
             <div className="px-[8rem] lg:px-[10rem] ">
@@ -28,8 +33,10 @@ function HotList({ breakfast }) {
                         // 當螢幕寬度 >= 640px 時
                         640: { slidesPerView: 2 },
 
+
                         // 當螢幕寬度 >= 768px 時
                         768: { slidesPerView: 3 },
+
 
                         // 當螢幕寬度 >= 1024px 時
                         1024: { slidesPerView: 4 },
@@ -43,14 +50,20 @@ function HotList({ breakfast }) {
                                 <HotItems food={food} />
                             </SwiperSlide>
 
+
                         ))}
                 </Swiper>
             </div>
 
 
+
+
         </div>
+
 
     )
 }
 
+
 export default HotList
+
--- src/pages/Map/MapComponent.jsx
+++ src/pages/Map/MapComponent.jsx
@@ -1,9 +1,10 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-export default function Map() {
+
+export default function MapComponent() {
   return (
-    <div className="w-full h-[300px] md:h-[500px] rounded-xl overflow-hidden shadow-lg">
+    <div className="relative z-10 w-full h-[300px] md:h-[500px] rounded-xl overflow-hidden shadow-lg">
       <MapContainer center={[25.024377585879215, 121.54677998700183]} zoom={20} scrollWheelZoom={true} className="w-full h-full">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -16,3 +17,6 @@ export default function Map() {
     </div>
   );
 }
+
+
+
